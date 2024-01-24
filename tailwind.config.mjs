1d6de9ec1a9a350a4aa1c2541ead1b0cc5b318c5
Jesús Castillo Vidal <jcdev2004@gmail.com>
2024-01-24 13:07:28 -0500

    v.0.0.3 Carousel must be done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      screens: {
+        ps: "430px",
+      },
+      fontFamily: {
+        "sf-regular": ["SFProRegular", "sans-serif"],
+        "sf-medium": ["SFProMedium", "sans-serif"],
+        "sf-semibold": ["SFProSemibold", "sans-serif"],
+        "sf-bold": ["SFProBold", "sans-serif"],
+      },
+      colors: {
+        primary: "#0050EF",
+        secondary: "#5D50C6",
+      },
+    },
+  },
+  plugins: [require("flowbite/plugin")],
+};
